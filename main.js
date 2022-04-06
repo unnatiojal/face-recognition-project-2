3a4a415af071d056863f1376296174b5920100a3
unnatiojal <85604066+unnatiojal@users.noreply.github.com>
2022-04-06 23:19:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,16 +6,16 @@ Webcam.set({
 })
 
 camera = document.getElementById("camera");
-Webcam.attach("#camera");
+Webcam.attach('#camera');
 
-function take_sanpshot() {
+function take_snapshot() {
     Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
     });
 }
 
 console.log('ml5 version:', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZbEXDe6my/',modelLoaded)
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/ZbEXDe6my/model.json',modelLoaded)
 
 function modelLoaded() {
     console.log('Modal Loaded');
