e0301a2a5c5f81f90cfe2c01ea6b43d81c887efc
unnatiojal <85604066+unnatiojal@users.noreply.github.com>
2022-04-06 22:01:55 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+Webcam.set({
+    width: 450,
+    height: 300,
+    image_format: 'jpg',
+    jpg_quality: 90,
+})
+
+camera = document.getElementById("camera");
+Webcam.attach("#camera");
+
+function take_sanpshot() {
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier(',modelLoaded')
+
+function modelLoaded() {
+    console.log('Modal Loaded');
+}
